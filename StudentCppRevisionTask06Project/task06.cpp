ab8220840ba76e6b1248816f55fb9e74dc672aaa
= <=>
2023-04-06 18:04:31 +0300

    task06

--- StudentCppRevisionTask06Project/task06.cpp
+++ StudentCppRevisionTask06Project/task06.cpp
@@ -27,8 +27,10 @@ bool task06(int x1, int y1, int x2, int y2) {
 		|| x1 <= 0 || x2 <= 0 || y1 <= 0 || y2 <= 0) {
 		return false;
 	}
-	if (x1 + 1 == x2 && y1 + 3 == y2 || x1 - 1 == x2 && y1 + 3 == y2
-		|| x1 + 1 == x2 && y1 - 3 == y2 || x1 - 1 == x2 && y1 - 3 == y2) {
+	if (x1 - 2 == x2 && y1 + 1 == y2 || x1 - 1 == x2 && y1 + 2 == y2
+		|| x1 + 1 == x2 && y1 + 2 == y2 || x1 + 2 == x2 && y1 + 1 == y2
+		|| x1 - 1 == x2 && y1 - 2 == y2 || x1 - 2 == x2 && y1 - 1 == y2
+		|| x1 + 1 == x2 && y1 - 2 == y2 || x1 + 2 == x2 && y1 - 1 == y2) {
 		return true;
 	}
 	return false;
